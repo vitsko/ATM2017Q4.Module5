7350ed8d44f5c94058608eb38887677497161f21
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2017-12-26 13:51:54 +0400

    1. Delete packages to WebDriver for Firefox, IE: delete code for these 2. Add res-file for check content press-release

--- TestPressReleases/TestNavigate.cs
+++ TestPressReleases/TestNavigate.cs
@@ -1,136 +1,133 @@
 ﻿using System;
-using NUnit;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
 using System.Configuration;
 using OpenQA.Selenium.Support.UI;
-using System.Text;
-using System.Threading;
 
 namespace TestPressReleases
 {
     [TestFixture]
     public class TestNavigate
     {
-        private IWebDriver driver;
+        private static ChromeDriver driver = new ChromeDriver();
         private static string listOfReleases = "//div[@class=\"pressrelease-list-widget\"]";
         private static string pressRelease = "//div[@role=\"tablist\"]";
 
-        private static string lastPressReleases = "(//div[contains(@class,'panel-default')])[24]";
+        private static string PressReleasesAfterLoading = "//div[contains(@class,'panel-collapsible')]";
 
         private static string buttonLoadMore = "//button[@data-bind=\"visible: CanLoadMore\"]";
 
         private static string cssSelectorAboutPR = string.Format("{0}{1}{2}{3}{4}",
                                                                   "#pressreleaselist-month-",
-                                                                  ConfigurationManager.AppSettings["monthOfPressReleaseWithImage"],
+                                                                  ConfigurationManager.AppSettings["MonthOfPressReleaseWithImage"],
                                                                   " > div:nth-child(",
-                                                                  ConfigurationManager.AppSettings["numberOfPressRealeseInMonth"],
+                                                                  ConfigurationManager.AppSettings["NumberOfPressRealeseInMonth"],
                                                                   ")");
 
-        private static string titlePR = string.Format("{0}{1}{2}",
-                                                       "//div[@id=\"pressreleaselist-month-",
-                                                        ConfigurationManager.AppSettings["monthOfPressReleaseWithImage"],
-                                                        "]/div[2]/div[1]/h2/span");
+        private static string datePR = string.Format("{0}{1}{2}",
+                                                       "#pressreleaselist-month-",
+                                                        ConfigurationManager.AppSettings["MonthOfPressReleaseWithImage"],
+                                                        " > div:nth-child(2) > div.panel-heading > h2 > span.date");
 
+        private static string titlePR = string.Format("{0}{1}{2}",
+                                                       "#pressreleaselist-month-",
+                                                        ConfigurationManager.AppSettings["MonthOfPressReleaseWithImage"],
+                                                        " > div:nth-child(2) > div.panel-heading > h2 > span.title");
+
+        private static int CountPressReleases = int.Parse(ConfigurationManager.AppSettings["CountPressReleases"]);
 
         private string baseUrl = ConfigurationManager.AppSettings["baseUrl"];
         private By listOfReleasesXPhath = By.XPath(listOfReleases);
         private By pressReleaseXPath = By.XPath(pressRelease);
 
-        private By lastPressReleasesXPath = By.XPath(lastPressReleases);
+        private By PressReleasesAfterLoadingXPath = By.XPath(PressReleasesAfterLoading);
 
         private By buttonLoadMoreXPath = By.XPath(buttonLoadMore);
-        //private By cssSelectorAboutPRXPath = By.XPath(cssSelectorAboutPR);
-        private By titlePRXPath = By.XPath(titlePR);
 
-        const int CountPressReleases = 10;
+        private int timeoutInSec = int.Parse(ConfigurationManager.AppSettings["TimeoutInSec"]);
 
-        private int timeoutInSec = int.Parse(ConfigurationManager.AppSettings["timeoutInSec"]);
+        private static IWebElement PressReleaseOnPageByCssSelector
+        {
+            get
+            {
+                return driver.FindElementByCssSelector(cssSelectorAboutPR);
+            }
+        }
 
         [SetUp]
         public void SetupTest()
         {
-            switch (TestContext.CurrentContext.Test.Name)
-            {
-                case "TestChrome":
-
-                    this.driver = new ChromeDriver();
-                    break;
-
-                case "TestFireFox":
-                    var service = FirefoxDriverService.CreateDefaultService();
-                    this.driver = new FirefoxDriver(service);
-                    break;
-
-                case "TestIE":
-                    this.driver = new InternetExplorerDriver();
-                    break;
-
-                default:
-                    break;
-            }
-
-            this.driver.Navigate().GoToUrl(this.baseUrl);
-            this.driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(this.baseUrl);
+            driver.Manage().Window.Maximize();
         }
 
         [TearDown]
         public void CleanUp()
         {
-            this.driver.Close();
-            this.driver.Quit();
+            driver.Close();
+            driver.Quit();
         }
 
         [Test]
         public void TestChrome()
         {
-            var chrome = this.driver as ChromeDriver;
-
             this.BlockIsVisible();
             this.CountOfPressReleases();
             this.CountOfPressReleasesAfterLoading();
-            // AboutPR(chrome);
-
+            this.AboutPR();
         }
 
         public void BlockIsVisible()
         {
             this.IsElementVisible(listOfReleasesXPhath);
-            Assert.IsTrue(this.driver.FindElement(listOfReleasesXPhath).Displayed);
+            Assert.IsTrue(driver.FindElement(listOfReleasesXPhath).Displayed);
         }
 
         private void CountOfPressReleases()
         {
-            Assert.IsTrue(this.driver.FindElements(pressReleaseXPath).Count == TestNavigate.CountPressReleases);
+            Assert.IsTrue(driver.FindElements(pressReleaseXPath).Count == TestNavigate.CountPressReleases);
         }
 
         private void CountOfPressReleasesAfterLoading()
         {
-            this.driver.FindElement(buttonLoadMoreXPath).Click();
-            this.IsElementVisible(lastPressReleasesXPath);
+            driver.FindElement(buttonLoadMoreXPath).Click();
+            this.IsElementVisible(buttonLoadMoreXPath);
 
-            Assert.IsTrue(this.driver.FindElements(pressReleaseXPath).Count == 2 * TestNavigate.CountPressReleases);
+            Assert.IsTrue(driver.FindElements(PressReleasesAfterLoadingXPath).Count == 2 * TestNavigate.CountPressReleases);
         }
 
-        //private void AboutPR(ChromeDriver chrome)
-        //{
-        //    var text = chrome.FindElementByCssSelector(cssSelectorAboutPR).FindElements(titlePRXPath).ToString();
-
-        //    string title;
-
-        //    //foreach (var item in collection)
-        //    //{
-
-        //    //}
-
-        //}
-
-        private void IsElementVisible(By element, int timeoutSecs = 5)
+        private void AboutPR()
         {
-            new WebDriverWait(this.driver, TimeSpan.FromSeconds(timeoutSecs)).Until(ExpectedConditions.ElementIsVisible(element));
+            this.CheckTitlePR();
+
+
+
+        }
+
+        private void CheckTitlePR()
+        {
+            var date = InnerTextOfElementFromCssSelector(By.CssSelector(TestNavigate.datePR));
+            var title = InnerTextOfElementFromCssSelector(By.CssSelector(TestNavigate.titlePR));
+
+            var text = string.Format("{0}{1}", date, title);
+
+            Assert.AreEqual(Resource.TitlePR, text);
+        }
+
+        private void CheckImagePR()
+        {
+
+        }
+
+        private string InnerTextOfElementFromCssSelector(By by)
+        {
+            return PressReleaseOnPageByCssSelector.FindElement(by).Text;
+        }
+
+        private void IsElementVisible(By element)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(this.timeoutInSec)).Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(element));
         }
 
 
