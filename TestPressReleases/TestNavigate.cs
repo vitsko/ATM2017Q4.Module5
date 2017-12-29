a719d1a80bd8fc19da3bb542ee5714690bfae32f
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2017-12-29 19:03:35 +0400

    Check all data for press-releases on the page except link to page of press-release

--- TestPressReleases/TestNavigate.cs
+++ TestPressReleases/TestNavigate.cs
@@ -1,75 +1,57 @@
-﻿using System;
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Configuration;
-using OpenQA.Selenium.Support.UI;
-
-namespace TestPressReleases
+﻿namespace TestPressReleases
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Configuration;
+    using System.Linq;
+    using System.Net;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+
     [TestFixture]
     public class TestNavigate
     {
-
-        /// <summary>
-        /// (//div[@role='tablist'])//div[@class='panel-body']//div[@class='image']//img
-        /// </summary>
         private static ChromeDriver driver = new ChromeDriver();
-        private static string listOfReleases = "//div[@class='pressrelease-list-widget']";
-        private static string pressRelease = "//div[@role='tablist']";
 
-        private static string PressReleasesAfterLoading = "//div[contains(@class,'panel-collapsible')]";
+        private static string baseUrl = ConfigurationManager.AppSettings["baseUrl"];
+        private static int timeoutInSec = int.Parse(ConfigurationManager.AppSettings["TimeoutInSec"]);
+        private static int countOfPressReleases = int.Parse(ConfigurationManager.AppSettings["CountOfPressReleases"]);
+        private static int countOfClickMoreLoad = int.Parse(ConfigurationManager.AppSettings["CountOfClickMoreLoad"]);
 
-        private static string buttonLoadMore = "//button[@data-bind='visible: CanLoadMore']";
+        private By listOfReleasesXPhath = By.XPath("//div[@class='pressrelease-list-widget']");
+        private By pressReleaseXPath = By.XPath("//div[@role='tablist']");
 
-        private static string CssSelectorAboutPR
+        private By pressReleasesAfterLoadingXPath = By.XPath("//div[contains(@class,'panel-collapsible')]");
+
+        private By buttonLoadMoreXPath = By.XPath("//button[@data-bind='visible: CanLoadMore']");
+
+        private string cssHeaderPR = "div.panel-default.panel-time-line.panel-collapsible>div.panel-heading>h2>span";
+        private string cssImagePR = "div.panel-default.panel-time-line.panel-collapsible div.image img";
+        private string srcImage = "src";
+
+        private string cssAnnouncement = "div.panel-default.panel-time-line.panel-collapsible div.tableOverflow";
+
+        private string cssLinkToWatchPDF = "div.panel-default.panel-time-line.panel-collapsible a.icon-s-flipbook-pdf_round";
+        private string cssLinkToDownloadPDF = "div.panel-default.panel-time-line.panel-collapsible a.icon-s-download_round";
+        private string href = "href";
+
+        private int CountOfElements { get; set; }
+
+        private int MaxCountOfPROnPage
         {
             get
             {
-                return string.Format("#{0}{1}", driver.FindElement(By.XPath("(//a[@class='point point-month'])[1]")).GetAttribute("aria-controls"), " > div:nth-child(1)");
-            }
-        }
-
-        private static string DatePR
-        {
-            get
-            {
-                return string.Format("#{0}{1}", driver.FindElement(By.XPath("(//a[@class='point point-month'])[1]")).GetAttribute("aria-controls"), " > div:nth-child(1) > div.panel-heading > h2 > span.date");
-            }
-        }
-
-        private static string TitlePR
-        {
-            get
-            {
-                return string.Format("#{0}{1}", driver.FindElement(By.XPath("(//a[@class='point point-month'])[1]")).GetAttribute("aria-controls"), " > div:nth-child(1) > div.panel-heading > h2 > span.title");
-            }
-        }
-
-        private static int CountPressReleases = int.Parse(ConfigurationManager.AppSettings["CountPressReleases"]);
-
-        private string baseUrl = ConfigurationManager.AppSettings["baseUrl"];
-        private By listOfReleasesXPhath = By.XPath(listOfReleases);
-        private By pressReleaseXPath = By.XPath(pressRelease);
-
-        private By PressReleasesAfterLoadingXPath = By.XPath(PressReleasesAfterLoading);
-
-        private By buttonLoadMoreXPath = By.XPath(buttonLoadMore);
-
-        private int timeoutInSec = int.Parse(ConfigurationManager.AppSettings["TimeoutInSec"]);
-
-        private static IWebElement PressReleaseOnPageByCssSelector
-        {
-            get
-            {
-                return driver.FindElementByCssSelector(CssSelectorAboutPR);
+                return (TestNavigate.countOfClickMoreLoad + 1) * TestNavigate.countOfPressReleases;
             }
         }
 
         [SetUp]
         public void SetupTest()
         {
-            driver.Navigate().GoToUrl(this.baseUrl);
+            driver.Navigate().GoToUrl(baseUrl);
             driver.Manage().Window.Maximize();
         }
 
@@ -84,61 +66,108 @@ namespace TestPressReleases
         public void TestListOfPressReleases()
         {
             Assert.IsTrue(this.BlockIsVisible());
-            Assert.AreEqual(this.CountOfPressReleases(), TestNavigate.CountPressReleases);
-            Assert.IsTrue(this.LoadMore());
+            Assert.AreEqual(this.DefaultCountOfPressReleases(), TestNavigate.countOfPressReleases);
+            Assert.IsTrue(this.LoadMore() <= this.MaxCountOfPROnPage);
             Assert.IsTrue(this.AboutPR());
         }
 
         public bool BlockIsVisible()
         {
-            this.IsElementVisible(listOfReleasesXPhath);
-            return driver.FindElement(listOfReleasesXPhath).Displayed;
+            this.IsElementVisible(this.listOfReleasesXPhath);
+            return driver.FindElement(this.listOfReleasesXPhath).Displayed;
         }
 
-        private int CountOfPressReleases()
+        private int DefaultCountOfPressReleases()
         {
-            return driver.FindElements(pressReleaseXPath).Count;
+            return driver.FindElements(this.pressReleaseXPath).Count;
         }
 
-        private bool LoadMore()
+        private int LoadMore()
         {
-            driver.FindElement(buttonLoadMoreXPath).Click();
-            this.IsElementVisible(buttonLoadMoreXPath);
+            try
+            {
+                for (int i = 0; i <= TestNavigate.countOfClickMoreLoad - 1; i++)
+                {
+                    this.IsElementVisible(this.buttonLoadMoreXPath);
+                    driver.FindElement(this.buttonLoadMoreXPath).Click();
+                    this.IsElementVisible(this.buttonLoadMoreXPath);
+                }
+            }
+            catch { }
 
-            return driver.FindElements(PressReleasesAfterLoadingXPath).Count == 2 * TestNavigate.CountPressReleases;
+            return driver.FindElements(this.pressReleasesAfterLoadingXPath).Count;
         }
 
         private bool AboutPR()
         {
-            return this.CheckTitlePR();
+            return this.IsExistTextByCSS(this.cssHeaderPR) && (this.CountOfElements / 2 <= this.MaxCountOfPROnPage)
+                   && this.CheckLinkOfFile(this.cssImagePR, this.srcImage) && (this.CountOfElements <= this.MaxCountOfPROnPage)
+                   && this.IsExistTextByCSS(this.cssAnnouncement) && (this.CountOfElements <= this.MaxCountOfPROnPage)
+                   && this.CheckLinkOfFile(this.cssLinkToWatchPDF, this.href) && (this.CountOfElements <= this.MaxCountOfPROnPage)
+                   && this.CheckLinkOfFile(this.cssLinkToDownloadPDF, this.href) && (this.CountOfElements <= this.MaxCountOfPROnPage);
         }
 
-        private bool CheckTitlePR()
+        private bool IsExistTextByCSS(string cssSelector)
         {
-            var date = InnerTextOfElementFromCssSelector(By.CssSelector(TestNavigate.DatePR));
-            var title = InnerTextOfElementFromCssSelector(By.CssSelector(TestNavigate.TitlePR));
-
-            return !(string.IsNullOrWhiteSpace(date) && string.IsNullOrWhiteSpace(title));
+            return this.InnerTextOfElementsFromCssSelector(cssSelector).TrueForAll(item => !string.IsNullOrWhiteSpace(item));
         }
 
-        private void CheckImagePR()
+        private List<string> InnerTextOfElementsFromCssSelector(string cssSelector)
         {
+            var text = new List<string>();
 
+            foreach (var item in this.GetAllElementsByCssSelector(cssSelector))
+            {
+                text.Add(item.Text);
+            }
+
+            this.CountOfElements = text.Count;
+
+            return text;
         }
 
-        private string InnerTextOfElementFromCssSelector(By by)
+        private bool CheckLinkOfFile(string cssSelector, string attribute)
         {
-            return PressReleaseOnPageByCssSelector.FindElement(by).Text;
+            var urls = this.GetValueOfAttribute(cssSelector, attribute);
+
+            WebRequest request;
+            HttpWebResponse response;
+
+            var sizes = new List<long>();
+
+            for (int i = 0; i < urls.Count; i++)
+            {
+                request = WebRequest.Create(urls.ElementAt(i));
+                response = (HttpWebResponse)request.GetResponse();
+
+                sizes.Add(response.ContentLength);
+            }
+
+            this.CountOfElements = sizes.Count;
+
+            return sizes.TrueForAll(size => size > 0);
+        }
+
+        private List<string> GetValueOfAttribute(string cssSelector, string attribute)
+        {
+            var value = new List<string>();
+
+            foreach (var item in this.GetAllElementsByCssSelector(cssSelector))
+            {
+                value.Add(item.GetAttribute(attribute));
+            }
+
+            return value;
+        }
+
+        private ReadOnlyCollection<IWebElement> GetAllElementsByCssSelector(string cssSelector)
+        {
+            return driver.FindElementsByCssSelector(cssSelector);
         }
 
         private void IsElementVisible(By element)
         {
-            new WebDriverWait(driver, TimeSpan.FromSeconds(this.timeoutInSec)).Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(element));
+            new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSec)).Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(element));
         }
-
-        //#pressreleaselist-month-0-2017-12 > div:nth-child(1)
-
     }
-
-
 }
